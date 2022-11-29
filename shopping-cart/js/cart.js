8afd165dd61e6a7f4ea0694c5d6c46fe4baae7dd
hamidashkh <111846798+hamidashkh@users.noreply.github.com>
2022-11-29 23:58:25 +0400

    Update cart.js

--- shopping-cart/js/cart.js
+++ shopping-cart/js/cart.js
@@ -16,43 +16,97 @@ ShowAlert();
 
 function GetList() {
     let basket = JSON.parse(localStorage.getItem('products'));
-
+    let total =document.getElementById('total');
+    // let int_price =document.getElementById('total');
+    let sum=0;
     let row = '';
     basket.forEach(pr => {
+        let int_price = pr.Price.slice(-(pr.Price.length),-4);
+        sum += +int_price;
        
         row += `
             <tr>
-                <th scope="row">${pr.Id}</th>
+                <th class="id" scope="row">${pr.Id}</th>
                 <td class="img-td">
                     <img src=${pr.Image} alt="">
                 </td>
                 <td>${pr.Name.length > 10 ? pr.Name.slice(0,20) + "..." : pr.Name}</td>
                 <td>
-                    <input type="number" id="inp" onchange="Price()" value=${pr.Count}>
+                    <input class="count" type="number" onchange="Price()" value=${pr.Count}>
                 </td>
                 <td>
-                <span class="text-success fw-bold" id="dollar"> AZN</span>
+                        <span  class="text-success fw-bold sum">${int_price * pr.Count} AZN</span>
                 </td>
+                <td>
+                        <button id=${pr.Id} onclick="DeletePr()" class="btn btn-warning delete" >sil</button>
+                </td>
+                
             </tr>
         `
+        Price();
+
     })
+    if (sum > 0) {
+        total.innerHTML = `Toplam : ${sum} AZN`;
+    }else{
+        total.style.display = "none";
+                
+    }
+    document.getElementById('tbdy').innerHTML = row;
 
     document.getElementById('tbdy').innerHTML = row;
 }
 
 GetList();
 
-let value=document.getElementById('inp').value;
+// let value=document.getElementById('inp').value;
 
 
 function Price(){
     let basket = JSON.parse(localStorage.getItem('products'));
-
-    basket.forEach(pr => {
-        let int_price = pr.Price.slice(-(pr.Price.length),-4);
-        let value=document.getElementById('inp').value;
-        let price=value*int_price;
-        document.getElementById('dollar').innerHTML = price + "AZN";
-});
+    let sum = document.querySelectorAll('.sum');
+    let count = document.querySelectorAll('.count');
+    let total = document.getElementById('total')
+    let total_sum = 0;
+    let x = 0;
+    let t = 0;
+    for (const i of count) {
+        if (x < basket.length) {
+            int_price = basket[x].Price.slice(-(basket[x].Price.length),-4)
+            basket[x].Count = i.value;
+            int_price = int_price * i.value;
+            total_sum += +int_price;
+            if(t < sum.length){
+                sum[t].innerHTML = int_price.toFixed(2) + " AZN"
+            }
+            t++
+            
+            
+        }
+        x++
+    }
+    if (total_sum > 0) {
+        total.innerHTML = `Toplam : ${total_sum.toFixed(2)} AZN`
+    }
+    localStorage.setItem('products',JSON.stringify(basket));
     
 }
+function DeletePr() {
+    let basket = JSON.parse(localStorage.getItem('products'));
+    let dlt = document.querySelectorAll('.delete')
+    let count = 0;
+        for (const i of dlt) {
+            i.addEventListener('click', function(){
+                for (const pr of basket) {
+                    if(pr.Id === i.id){
+                        basket.splice(count,1)
+                        localStorage.setItem('products',JSON.stringify(basket))
+                        GetList();
+                        break
+                    }
+                count++
+            }
+        },true)
+    }
+}
+DeletePr();
